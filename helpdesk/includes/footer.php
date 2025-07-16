f6c2e314f1fd8c7c6c51836c4fb93f36b24f328e
vinicius-althm <vinicius.altm@gmail.com>
2025-07-16 20:05:57 -0300

    Footer

---
+++ helpdesk/includes/footer.php
@@ -0,0 +1,13 @@
+ 
+  </main>
+
+
+<footer class="mt-4 bg-dark text-center text-muted text-lg-start ">
+  <div class="text-center p-3 text-muted">
+          <span>By </span>
+
+    <a class="d-inline-flex align-items-center text-muted" href="https://github.com/vinicius-althm">
+      Vinicius Altheman
+    </a>
+  </div>
+</footer>
