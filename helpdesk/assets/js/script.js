9303435f418f2de2447e3f98f5799dc0215a7477
vinicius-althm <vinicius.altm@gmail.com>
2025-07-16 20:07:21 -0300

    Configuração css,js,images

---
+++ helpdesk/assets/js/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+  $("#tab_consulta").DataTable({
+    
+    dom:
+      "<'row mb-3'<'col-sm-12 col-md-6'B><'col-sm-12 col-md-6'f>>" +
+      "<'row'<'col-sm-12'tr>>" +
+      "<'row mt-3'<'col-sm-12 col-md-7 'i><'col-sm-12 col-md-5 flex-wrap  justify-content-center 'p>>",
+    scrollY: 400,
+    buttons: [
+      {
+        text: "Excel",
+        extend: "excel",
+        className: "me-3",
+      },
+      {
+        text: "PDF",
+        extend: "pdf",
+        className: "btn btn-danger",
+        orientation: "landscape",
+        portrait: "A1",
+      },
+    ],
+    language: {
+      lengthMenu: "Mostrar _MENU_ registros por página",
+      zeroRecords: "Nada encontrado ❌",
+      info: "Páginas _PAGE_ de _PAGES_",
+      infoEmpty: "Nenhum registro disponível",
+      infoFiltered: "(filtrado de _MAX_ registros)",
+      search: "Buscar:",
+      paginate: {
+        previous: "Anterior",
+        next: "Próxima",
+      },
+    },
+  });
+});
